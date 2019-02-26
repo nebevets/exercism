e3839b21f2c70d25c402a4accf6139a5b0f75a3f
nebevets <steveben@exmsft.com>
2019-02-26 09:36:55 -0800

    inital tests pass

---
+++ javascript/all-your-base/all-your-base.js
@@ -0,0 +1,27 @@
+export const convert = (array, fromBase, toBase) => {
+    if (fromBase <= 1 || toBase <= 1) throw new Error(`Wrong ${fromBase <= 1 ? 'input' : 'output'} base`);
+    if (array.length === 0) throw new Error('Input has wrong format');
+    if (array.length > 1 && array[0] === 0) throw new Error('Input has wrong format');
+    if (fromBase === toBase) return array;
+    let arrayLength = array.length - 1;
+    let number = null;
+    const result = [];
+    for(let index = 0; index < array.length; index++, arrayLength--){
+        if (array[index] < 0 || array[index] >= fromBase) throw new Error('Input has wrong format');
+        number += array[index] * (fromBase**arrayLength);
+    }
+    let power = 0;
+    let calc = null;
+    do{
+        calc = Math.floor(number/(toBase**power++));
+    }while(calc >= toBase);
+    
+    while(power > 0){
+        --power;
+        let quotient = Math.floor(number/(toBase**power));
+        let remainder = number % (toBase**power);
+        result.push(quotient);
+        number = remainder;
+    }
+    return result;
+}
