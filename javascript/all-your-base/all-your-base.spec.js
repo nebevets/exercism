277eb904a554b87ccc4f1ec052dbbf477309c37d
nebevets <steveben@exmsft.com>
2019-02-26 10:31:37 -0800

    finised error checking

--- javascript/all-your-base/all-your-base.spec.js
+++ javascript/all-your-base/all-your-base.spec.js
@@ -109,25 +109,25 @@ describe('Converter', () => {
     }).toThrow(new Error('Wrong input base'));
   });
 
-  xtest('missing input base throws an error', () => {
+  test('missing input base throws an error', () => {
     expect(() => {
       convert([0]);
     }).toThrow(new Error('Wrong input base'));
   });
 
-  xtest('wrong input_base base not integer', () => {
+  test('wrong input_base base not integer', () => {
     expect(() => {
       convert([0], 2.5);
     }).toThrow(new Error('Wrong input base'));
   });
 
-  xtest('missing output base throws an error', () => {
+  test('missing output base throws an error', () => {
     expect(() => {
       convert([0], 2);
     }).toThrow(new Error('Wrong output base'));
   });
 
-  xtest('wrong output_base base not integer', () => {
+  test('wrong output_base base not integer', () => {
     expect(() => {
       convert([0], 3, 2.5);
     }).toThrow(new Error('Wrong output base'));
