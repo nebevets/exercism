4f1549caf676f507dd9e0df0ee5f7ee73001e501
nebevets <steveben@exmsft.com>
2019-05-10 11:52:50 -0700

    bob exercise complete

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -6,8 +6,12 @@ export const hey = (message) => {
   if(!words.length) return 'Fine. Be that way!';
   const lastWord = words[words.length - 1];
   const isQuestion = lastWord[lastWord.length - 1] === '?';
-  const numUpperCaseWords = (words.filter(word => /([A-Z]+)/.test(word)));
-  const numOtherWords = (words.filter(word => /([a-z]+)/.test(word)));
+  const numUpperCaseWords = words
+    .filter(word => /\b[A-Z]+\b/.test(word))
+    .length;
+  const numOtherWords = words
+    .filter(word => /[a-z]+/.test(word))
+    .length;
   const isMostlyUpperCase = numUpperCaseWords > numOtherWords;
   if(isQuestion && isMostlyUpperCase) return 'Calm down, I know what I\'m doing!';
   else if(isMostlyUpperCase) return 'Whoa, chill out!';
