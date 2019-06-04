21b9c18e478973a41c2bca0d7d99cc13af6bb3a1
nebevets <steveben@exmsft.com>
2019-06-04 13:35:47 -0700

    changed code per mentor feedback

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -1,20 +1,11 @@
 export const hey = (message) => {
-  const words = message
-    .replace(/\d/g, '')
-    .split(/\s/)
-    .filter(onlyChars => onlyChars);
-  if(!words.length) return 'Fine. Be that way!';
-  const lastWord = words[words.length - 1];
-  const isQuestion = lastWord[lastWord.length - 1] === '?';
-  const numUpperCaseWords = words
-    .filter(word => /\b[A-Z]+\b/.test(word))
-    .length;
-  const numOtherWords = words
-    .filter(word => /[a-z]+/.test(word))
-    .length;
-  const isMostlyUpperCase = numUpperCaseWords > numOtherWords;
-  if(isQuestion && isMostlyUpperCase) return 'Calm down, I know what I\'m doing!';
-  else if(isMostlyUpperCase) return 'Whoa, chill out!';
+  const messageString = message.trim();
+  if(!messageString.length) return 'Fine. Be that way!';
+  const isQuestion = messageString.endsWith('?');
+  const isShouting = messageString === messageString.toUpperCase();
+  const hasLetters = messageString.match(/[A-Z]/gi);
+  if(hasLetters && isShouting && isQuestion) return 'Calm down, I know what I\'m doing!';
+  else if(hasLetters && isShouting) return 'Whoa, chill out!';
   else if(isQuestion) return 'Sure.';
   return 'Whatever.';
 };
