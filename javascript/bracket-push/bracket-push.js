a59b2e66ba1fdacd423cae0222b947b8bcd9340a
nebevets <steveben@exmsft.com>
2019-06-12 09:44:45 -0700

    bracket push

--- javascript/bracket-push/bracket-push.js
+++ javascript/bracket-push/bracket-push.js
@@ -1,26 +1,17 @@
-export const bracketPush = (string) => {
-  var charArray = string.split('');
-  var pairsArrays = [
-      ['(', ')'],
-      ['{', '}'],
-      ['[', ']']
-  ]
-  for(var index = 0; index < pairsArrays.length; index++){
-      var test1 = charArray.indexOf(pairsArrays[index][0]);
-      var test2 = charArray.indexOf(pairsArrays[index][1]);
-      if (test1 !== -1 && test2 !== -1){
-          if (test1 < test2){
-              charArray.splice(test2, 1);
-              charArray.splice(test1, 1);
-              index = 0;
-          }else{
-              return false;
-          }
-      }else if(test1 === -1 && test2 !== -1){
-          return false;
-      }else if(test2 === -1 && test1 !== -1){
-          return false;
-      }
-  }
-  return true;
-}
+export const bracketPush = string => {
+    const closingBracketStack = [];
+    return string
+      .split('')
+      .every(bracket => {
+        switch (bracket) {
+          case "(": return closingBracketStack.push(")");
+          case "[": return closingBracketStack.push("]");
+          case "{": return closingBracketStack.push("}");
+          case ")":
+          case "]":
+          case "}":
+            return closingBracketStack.pop() === bracket;
+        }
+      }) && !closingBracketStack.length;
+  };
+  
