4dfe056770197ddcc81c504b8baf0b0f5ac948ab
nebevets <steveben@exmsft.com>
2019-07-09 13:22:29 -0700

    pascals triangle class without helper function, non-recursive

--- javascript/pascals-triangle/pascals-triangle.js
+++ javascript/pascals-triangle/pascals-triangle.js
@@ -1,31 +1,23 @@
-const pascalsTriangle = (rows, result=[], resultIndex=0) => {
-  const row = [1];
-  const nextStart = result.length;
-  let index = 0;
-  while(index < result.length){
-      if(result[resultIndex][index + 1]){
-          row.push(result[resultIndex][index] + result[resultIndex][index + 1]);
-      }else{
-          row.push(1);
-      }
-      index++;
-  }
-  result.push(row);
-  rows--;
-  if(rows > 0){
-    result = pascalsTriangle(rows, result, nextStart);
-  }
-  return result;
-} 
-
 export class Triangle{
   constructor(rows){
-    this.rows = null;
-    this.lastRow = null;
-    this._makeRows(rows);
-  }
-  _makeRows(rows){
-    this.rows = pascalsTriangle(rows);
+    this.rows = this._pascalsTriangle(rows);
     this.lastRow = this.rows[this.rows.length - 1];
   }
+  _pascalsTriangle(rows){
+    const result=[];
+    do{
+      let index = 0, resultIndex = result.length-1;
+      const row = [1];
+      while(index < result.length){
+        if(result[resultIndex][index + 1]){
+            row.push(result[resultIndex][index] + result[resultIndex][index + 1]);
+        }else{
+            row.push(1);
+        }
+        index++;
+      }
+      result.push(row);
+    } while(--rows > 0);
+    return result;
+  } 
 }
